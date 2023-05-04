de5fbf4dfd1c4d68ad1eefb61631aaa8a4de23fc
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-05-04 18:27:20 +0600

    environment variabe hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable', import.meta.env.VITE_apiKey)
 const firebaseConfig = {
-  apiKey: "AIzaSyD8PAu0NEbmWWZbznSYSf8SlwltJuOnLoU",
-  authDomain: "food-and-taste-recipe.firebaseapp.com",
-  projectId: "food-and-taste-recipe",
-  storageBucket: "food-and-taste-recipe.appspot.com",
-  messagingSenderId: "907364333845",
-  appId: "1:907364333845:web:89330854369587ba26511c"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
